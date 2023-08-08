13be68b8783176995eae2aa121bdc9c56bc2570b
Jamal <jamal.hyderr@gmail.com>
2023-08-08 11:34:44 +0500

    app settings updated

--- appSettings.js
+++
@@ -1,3 +0,0 @@
-export const appSettings = {
-  databaseURL: "https://react-notes-26235-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-import { appSettings } from "./appSettings.js"
+import { appSettings } from "./app-settings.js"
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
